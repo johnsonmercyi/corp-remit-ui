71838a5fc16f7cb0315c2c4ad5c60aa5d49c1820
Johnson Mercy I <you@example.com>
2021-05-19 12:57:20 +0100

    Added Layout Component

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Layout Component
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+import style from './Layout.module.css'
+
+class Layout extends Component {
+    state = {}
+    render() {
+        return (
+            <div className={style.Layout}></div>
+        );
+    }
+}
+
+export default Layout;
